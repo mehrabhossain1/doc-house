00ae0d555ebf1d0da6efa7e5536ad3de30810a91
mehrabhossain1 <mehrab.munna00@gmail.com>
2023-12-30 12:42:47 +0600

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Footer } from './shared/Footer'
+import { Header } from './shared/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/shared/Header.jsx
@@ -0,0 +1,26 @@
+import logo from '../assets/logo.svg'
+
+export const Header = () => {
+  return (
+    <div className='bg-[#07332F] w-[100%] h-[800px]'>
+      {/* logo */}
+      <div className='flex'>
+        <img src={logo} alt='' />
+        <p className='text-4xl font-bold'>
+          <span className='text-[#F7A582]'>Doc</span>{' '}
+          <span className='text-white'>House</span>
+        </p>
+      </div>
+
+      {/* nav */}
+      <div>
+        <ul className='flex'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Appointment</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
